287c539a793ae415f08e14b27a84b5cf1ee77fc8
mohamedmorsey <mohamedmorsey@semmtech.nl>
2018-11-29 19:48:58 +0100

    Extended integration tests with player 2 wins

--- kalah/src/test/java/com/backbase/kalah/rest/integration/GameRestControllerIT.java
+++ kalah/src/test/java/com/backbase/kalah/rest/integration/GameRestControllerIT.java
@@ -10,7 +10,7 @@ import com.backbase.kalah.repository.PitRepository;
 import com.backbase.kalah.rest.GameRestController;
 import com.backbase.kalah.service.BoardService;
 import com.backbase.kalah.service.GameService;
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -19,22 +19,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.inject.Inject;
-
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.backbase.kalah.constant.Constants.PLAYER_1_KALAH;
+import static com.backbase.kalah.constant.Constants.PLAYER_2_KALAH;
 import static com.backbase.kalah.constant.Fields.PIT_ID_PARAMETER;
 import static com.backbase.kalah.constant.Paths.GAMES_CONTEXT_PATH;
 import static com.backbase.kalah.constant.Paths.PITS_CONTEXT_PATH;
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpMethod.POST;
 import static org.springframework.http.HttpMethod.PUT;
-import org.apache.commons.io.IOUtils;
 
 /**
  * Integration test class for {@link GameRestController}
@@ -48,9 +51,11 @@ import org.apache.commons.io.IOUtils;
 public class GameRestControllerIT {
 
     private static final ParameterizedTypeReference<GameDto> GAME_DTO_RESPONSE_TYPE =
-            new ParameterizedTypeReference<GameDto>() {};
+            new ParameterizedTypeReference<GameDto>() {
+            };
     private static final ParameterizedTypeReference<GameStatusDto> GAME_STATUS_DTO_RESPONSE_TYPE =
-            new ParameterizedTypeReference<GameStatusDto>() {};
+            new ParameterizedTypeReference<GameStatusDto>() {
+            };
 
     //region constants
     private static final String PLAYER1_WINNING_SEQUENCE_FILENAME = "player1-winning-sequence";
@@ -77,16 +82,6 @@ public class GameRestControllerIT {
 
     private UriComponentsBuilder builder;
 
-    @Before
-    public void setUp() throws Exception {
-//        testBoard = boardService.initBoard();
-//        testBoard.setId(BOARD_ID);
-//
-//        testGame = new Game(testBoard, GAME_URI);
-//        testGame.setId(GAME_ID);
-//        this.mockMvc = MockMvcBuilders.standaloneSetup(gameRestController)
-//                .setControllerAdvice(new KalahGameExceptionHandler()).build();
-    }
 
     @After
     public void teardown() {
@@ -98,13 +93,9 @@ public class GameRestControllerIT {
     /**
      * Tests the following scenario
      * <ul>
-     * <li>Get number of customers, accounts and transactions in the system</li>
-     * <li>Add customer</li>
-     * <li>Get number of customers after add</li>
-     * <li>Get number of accounts</li>
-     * <li>Add account</li>
-     * <li>Get number of accounts after add</li>
-     * <li>Get number of transactions after add</li>
+     * <li>Create a game</li>
+     * <li>Play a sequence of moves</li>
+     * <li>End result is that player1 wins</li>
      * </ul>
      */
     @Test
@@ -117,30 +108,89 @@ public class GameRestControllerIT {
         builder = UriComponentsBuilder.fromUriString("/" + GAMES_CONTEXT_PATH);
         String uri = builder.toUriString();
 
-//        MvcResult gameCreationResult = this.mockMvc.perform(post(uri))
-//                .andExpect(status().isCreated())
-//                .andExpect(jsonPath("$." + ID_FIELD).value(GAME_ID)).andReturn();
-//
-//        String content = gameCreationResult.getResponse().get
-
         ResponseEntity<GameDto> gameCreationResult =
                 restTemplate.exchange(uri, POST, HttpEntity.EMPTY, GAME_DTO_RESPONSE_TYPE);
+
+        // Check that the game has been created successfully
+        assertThat(gameCreationResult.getStatusCode().value()).isEqualTo(HttpServletResponse.SC_CREATED);
+        assertThat(gameCreationResult.getBody()).isNotNull();
+        assertThat(gameCreationResult.getHeaders().get(HttpHeaders.LOCATION).get(0))
+                .isEqualTo(gameCreationResult.getBody().getUri());
+
         String gameUri = gameCreationResult.getBody().getUri();
 
-
-        String uriTemplate = gameUri + "/" + PITS_CONTEXT_PATH + "/{pitId}";
+        String uriTemplate = gameUri + "/" + PITS_CONTEXT_PATH + "/{" + PIT_ID_PARAMETER + "}";
         Map<String, String> uriParams = new HashMap<>();
         builder = UriComponentsBuilder.fromUriString(uriTemplate);
+        ResponseEntity<GameStatusDto> gamePlayResult = null;
 
-        for (String pitPath:playSequence) {
+        for (String pitPath : playSequence) {
             uriParams.put(PIT_ID_PARAMETER, pitPath);
             uri = builder.buildAndExpand(uriParams).toUriString();
 
-            ResponseEntity<GameStatusDto> gamePlayResult =
-                    restTemplate.exchange(uri, PUT, HttpEntity.EMPTY, GAME_STATUS_DTO_RESPONSE_TYPE);
-
-            System.out.println(gamePlayResult.getBody());
+            gamePlayResult = restTemplate.exchange(uri, PUT, HttpEntity.EMPTY, GAME_STATUS_DTO_RESPONSE_TYPE);
         }
 
+        // Check that player1 has higher score
+        assertThat(gamePlayResult).isNotNull();
+        assertThat(gamePlayResult.getStatusCode().value()).isEqualTo(HttpServletResponse.SC_OK);
+
+        // Get scores of both players
+        String player1Score  = gamePlayResult.getBody().getStatus().get(String.valueOf(PLAYER_1_KALAH + 1));
+        String player2Score  = gamePlayResult.getBody().getStatus().get(String.valueOf(PLAYER_2_KALAH + 1));
+
+        assertThat(Integer.parseInt(player1Score)).isGreaterThan(Integer.parseInt(player2Score));
+    }
+
+    /**
+     * Tests the following scenario
+     * <ul>
+     * <li>Create a game</li>
+     * <li>Play a sequence of moves</li>
+     * <li>End result is that player2 wins</li>
+     * </ul>
+     */
+    @Test
+    public void testCreateAndPlayGameWithPlayer2Wins() throws Exception {
+        String player1WinningSequence = IOUtils.toString(
+                this.getClass().getResourceAsStream(PLAYER2_WINNING_SEQUENCE_FILENAME), UTF_8);
+        String[] playSequence = player1WinningSequence.split("\n");
+
+        // Prepare the correct URI to contact the game REST controller and create the game
+        builder = UriComponentsBuilder.fromUriString("/" + GAMES_CONTEXT_PATH);
+        String uri = builder.toUriString();
+
+        ResponseEntity<GameDto> gameCreationResult =
+                restTemplate.exchange(uri, POST, HttpEntity.EMPTY, GAME_DTO_RESPONSE_TYPE);
+
+        // Check that the game has been created successfully
+        assertThat(gameCreationResult.getStatusCode().value()).isEqualTo(HttpServletResponse.SC_CREATED);
+        assertThat(gameCreationResult.getBody()).isNotNull();
+        assertThat(gameCreationResult.getHeaders().get(HttpHeaders.LOCATION).get(0))
+                .isEqualTo(gameCreationResult.getBody().getUri());
+
+        String gameUri = gameCreationResult.getBody().getUri();
+
+        String uriTemplate = gameUri + "/" + PITS_CONTEXT_PATH + "/{" + PIT_ID_PARAMETER + "}";
+        Map<String, String> uriParams = new HashMap<>();
+        builder = UriComponentsBuilder.fromUriString(uriTemplate);
+        ResponseEntity<GameStatusDto> gamePlayResult = null;
+
+        for (String pitPath : playSequence) {
+            uriParams.put(PIT_ID_PARAMETER, pitPath);
+            uri = builder.buildAndExpand(uriParams).toUriString();
+
+            gamePlayResult = restTemplate.exchange(uri, PUT, HttpEntity.EMPTY, GAME_STATUS_DTO_RESPONSE_TYPE);
+        }
+
+        // Check that player1 has higher score
+        assertThat(gamePlayResult).isNotNull();
+        assertThat(gamePlayResult.getStatusCode().value()).isEqualTo(HttpServletResponse.SC_OK);
+
+        // Get scores of both players
+        String player1Score  = gamePlayResult.getBody().getStatus().get(String.valueOf(PLAYER_1_KALAH + 1));
+        String player2Score  = gamePlayResult.getBody().getStatus().get(String.valueOf(PLAYER_2_KALAH + 1));
+
+        assertThat(Integer.parseInt(player2Score)).isGreaterThan(Integer.parseInt(player1Score));
     }
 }
